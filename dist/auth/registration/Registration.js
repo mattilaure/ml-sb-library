4b1662ff8b2511178a762ed2589624dbff384174
mattia laurenzana <mattia.laurenzana@beije.it>
2022-11-28 12:47:18 +0100

    Fix

---
+++ dist/auth/registration/Registration.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function Registration() {
+  return /*#__PURE__*/_react.default.createElement("div", null, "Registration");
+}
+var _default = Registration;
+exports.default = _default;
---
+++ dist/lobby/Lobby.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function Lobby() {
+  return /*#__PURE__*/_react.default.createElement("div", null, "Lobby");
+}
+var _default = Lobby;
+exports.default = _default;
