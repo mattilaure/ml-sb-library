b441e2037d8fafa11f458889a77fadadb30b26c9
SimonBreve <simone.brevetti@beije.it>
2022-12-01 12:30:09 +0100

    Cards added and renamed

---
+++ src/components/assets/images/index.js
@@ -0,0 +1,83 @@
+import BASTONI1 from "./1BAS.jpg";
+import BASTONI2 from "./2BAS.jpg";
+import BASTONI3 from "./3BAS.jpg";
+import BASTONI4 from "./4BAS.jpg";
+import BASTONI5 from "./5BAS.jpg";
+import BASTONI6 from "./6BAS.jpg";
+import BASTONI7 from "./7BAS.jpg";
+import BASTONIFANTE from "./8BAS.jpg";
+import BASTONICAVALLO from "./9BAS.jpg";
+import BASTONIRE from "./10BAS.jpg";
+import DENARI1 from "./1DEN.jpg";
+import DENARI2 from "./2DEN.jpg";
+import DENARI3 from "./3DEN.jpg";
+import DENARI4 from "./4DEN.jpg";
+import DENARI5 from "./5DEN.jpg";
+import DENARI6 from "./6DEN.jpg";
+import DENARI7 from "./7DEN.jpg";
+import DENARIFANTE from "./8DEN.jpg";
+import DENARICAVALLO from "./9DEN.jpg";
+import DENARIRE from "./10DEN.jpg";
+import COPPE1 from "./1COP.jpg";
+import COPPE2 from "./2COP.jpg";
+import COPPE3 from "./3COP.jpg";
+import COPPE4 from "./4COP.jpg";
+import COPPE5 from "./5COP.jpg";
+import COPPE6 from "./6COP.jpg";
+import COPPE7 from "./7COP.jpg";
+import COPPEFANTE from "./8COP.jpg";
+import COPPECAVALLO from "./9COP.jpg";
+import COPPERE from "./10COP.jpg";
+import SPADE1 from "./1SPA.jpg";
+import SPADE2 from "./2SPA.jpg";
+import SPADE3 from "./3SPA.jpg";
+import SPADE4 from "./4SPA.jpg";
+import SPADE5 from "./5SPA.jpg";
+import SPADE6 from "./6SPA.jpg";
+import SPADE7 from "./7SPA.jpg";
+import SPADEFANTE from "./8SPA.jpg";
+import SPADECAVALLO from "./9SPA.jpg";
+import SPADERE from "./10SPA.jpg";
+
+export const CARDS = [
+  BASTONI1,
+  BASTONI2,
+  BASTONI3,
+  BASTONI4,
+  BASTONI5,
+  BASTONI6,
+  BASTONI7,
+  BASTONIFANTE,
+  BASTONICAVALLO,
+  BASTONIRE,
+  DENARI1,
+  DENARI2,
+  DENARI3,
+  DENARI4,
+  DENARI5,
+  DENARI6,
+  DENARI7,
+  DENARIFANTE,
+  DENARICAVALLO,
+  DENARIRE,
+  COPPE1,
+  COPPE2,
+  COPPE3,
+  COPPE4,
+  COPPE5,
+  COPPE6,
+  COPPE7,
+  COPPEFANTE,
+  COPPECAVALLO,
+  COPPERE,
+  SPADE1,
+  SPADE2,
+  SPADE3,
+  SPADE4,
+  SPADE5,
+  SPADE6,
+  SPADE7,
+  SPADEFANTE,
+  SPADECAVALLO,
+  SPADERE,
+];
